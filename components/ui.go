1666020e671aed105ce98d073d5297d6bc2f3935
x-hgg-x <39058530+x-hgg-x@users.noreply.github.com>
2020-05-21 23:10:14 +0200

    Add ComputeDotOffset helper function

--- components/ui.go
+++ components/ui.go
@@ -1,6 +1,7 @@
 package components
 
 import (
+	"fmt"
 	"image/color"
 
 	"github.com/x-hgg-x/goecsengine/math"
@@ -14,6 +15,8 @@ type Text struct {
 	Text     string
 	FontFace font.Face
 	Color    color.RGBA
+	OffsetX  int
+	OffsetY  int
 }
 
 // Pivot variants
@@ -42,4 +45,37 @@ type MouseReactive struct {
 	ID          string
 	Hovered     bool
 	JustClicked bool
+}
+
+// ComputeDotOffset computes dot offset from text and pivot
+func ComputeDotOffset(text string, fontFace font.Face, pivot string) (x, y int, err error) {
+	bounds, _ := font.BoundString(fontFace, text)
+	centerX := ((bounds.Min.X + bounds.Max.X) / 2).Round()
+	centerY := ((bounds.Min.Y + bounds.Max.Y) / 2).Round()
+
+	switch pivot {
+	case TopLeft:
+		x, y = bounds.Min.X.Floor(), bounds.Min.Y.Floor()
+	case TopMiddle:
+		x, y = centerX, bounds.Min.Y.Floor()
+	case TopRight:
+		x, y = bounds.Max.X.Ceil(), bounds.Min.Y.Floor()
+	case MiddleLeft:
+		x, y = bounds.Min.X.Floor(), centerY
+	case Middle:
+		x, y = centerX, centerY
+	case MiddleRight:
+		x, y = bounds.Max.X.Ceil(), centerY
+	case BottomLeft:
+		x, y = bounds.Min.X.Floor(), bounds.Max.Y.Ceil()
+	case BottomMiddle:
+		x, y = centerX, bounds.Max.Y.Ceil()
+	case BottomRight:
+		x, y = bounds.Max.X.Ceil(), bounds.Max.Y.Ceil()
+	case "": // Middle
+		x, y = centerX, centerY
+	default:
+		err = fmt.Errorf("unknown pivot value: %s", pivot)
+	}
+	return
 }
--- systems/ui/render.go
+++ systems/ui/render.go
@@ -1,8 +1,6 @@
 package uisystem
 
 import (
-	"fmt"
-
 	c "github.com/x-hgg-x/goecsengine/components"
 	"github.com/x-hgg-x/goecsengine/utils"
 	w "github.com/x-hgg-x/goecsengine/world"
@@ -10,7 +8,6 @@ import (
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/text"
 	ecs "github.com/x-hgg-x/goecs/v2"
-	"golang.org/x/image/font"
 )
 
 // RenderUISystem draws text entities
@@ -19,39 +16,12 @@ func RenderUISystem(world w.World, screen *ebiten.Image) {
 		textData := world.Components.Engine.Text.Get(entity).(*c.Text)
 		uiTransform := world.Components.Engine.UITransform.Get(entity).(*c.UITransform)
 
-		bounds, _ := font.BoundString(textData.FontFace, textData.Text)
-		centerX := ((bounds.Min.X + bounds.Max.X) / 2).Round()
-		centerY := ((bounds.Min.Y + bounds.Max.Y) / 2).Round()
-
-		// Compute dot offset from pivot
-		var x, y int
-		switch uiTransform.Pivot {
-		case c.TopLeft:
-			x, y = bounds.Min.X.Floor(), bounds.Min.Y.Floor()
-		case c.TopMiddle:
-			x, y = centerX, bounds.Min.Y.Floor()
-		case c.TopRight:
-			x, y = bounds.Max.X.Ceil(), bounds.Min.Y.Floor()
-		case c.MiddleLeft:
-			x, y = bounds.Min.X.Floor(), centerY
-		case c.Middle:
-			x, y = centerX, centerY
-		case c.MiddleRight:
-			x, y = bounds.Max.X.Ceil(), centerY
-		case c.BottomLeft:
-			x, y = bounds.Min.X.Floor(), bounds.Max.Y.Ceil()
-		case c.BottomMiddle:
-			x, y = centerX, bounds.Max.Y.Ceil()
-		case c.BottomRight:
-			x, y = bounds.Max.X.Ceil(), bounds.Max.Y.Ceil()
-		case "": // Middle
-			x, y = centerX, centerY
-		default:
-			utils.LogError(fmt.Errorf("unknown pivot value: %s", uiTransform.Pivot))
-		}
+		// Compute dot offset
+		x, y, err := c.ComputeDotOffset(textData.Text, textData.FontFace, uiTransform.Pivot)
+		utils.LogError(err)
 
 		// Draw text
 		screenHeight := world.Resources.ScreenDimensions.Height
-		text.Draw(screen, textData.Text, textData.FontFace, uiTransform.Translation.X-x, screenHeight-uiTransform.Translation.Y-y, textData.Color)
+		text.Draw(screen, textData.Text, textData.FontFace, uiTransform.Translation.X+textData.OffsetX-x, screenHeight-uiTransform.Translation.Y-textData.OffsetY-y, textData.Color)
 	}))
 }
